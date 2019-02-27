569ed642c085df69606e0172e4051deef9631ca3
Ryan Moore <rhcmoore@gmail.com>
2019-02-27 13:42:30 -0800

    Convert to React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,13 @@ class Person extends Component {
     render() {
         return (
             // Using Aux as a wrapper to return a single expression
-            <Aux>
+            <React.Fragment>
             {/* // <div className={classes.Person}> */}
                 <p key="i1" onClick={this.props.click}>Me llamo {this.props.name} and I'm {this.props.age} </p>
                 <p key="i2">{this.props.children}</p>
                 <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
             {/* // </div> */}
-            </Aux>
+            </React.Fragment>
         )
     }
 }
